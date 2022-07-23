478c4b56687bfbf961c79440a024a28b8900bbfa
Praveen1692 <61341264+Praveen1692@users.noreply.github.com>
2022-07-23 09:09:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,45 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Welcome from "./Components/Welcome";
+
+
+
+import React from "react";
+
+
+
+
+function App() {
+
+  return (
+    <>
+   
+
+ 
+  
+  
+  <Welcome name="Praveen" />
+
+  
+
+  
+  
+   
+
+   
+  
+
+   
+
+  
+
+
+  
+   
+
+
+    </>
+  );
+}
+
+export default App;
